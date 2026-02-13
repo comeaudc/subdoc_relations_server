80fad4d70697912e04ef0efd2e3598d4321205c4
DylanC <comeaudc@comcast.net>
2026-02-13 14:00:57 -0500

    UPdated Cart SChema

--- models/cartSchema.js
+++ models/cartSchema.js
@@ -1,5 +1,16 @@
 import mongoose from "mongoose";
 
-const cartSchema = new mongoose.Schema({});
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    items: [cartItemSchema],
+  },
+  { timestamps: true },
+);
 
 export default mongoose.model("Cart", cartSchema);
